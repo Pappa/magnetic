1f64428e793b6aa0ac1af1f7f67982374f454168
Pappa <dr.pappa@gmail.com>
2019-02-27 21:30:42 +0000

    API gateway integration

--- handler.js
+++ handler.js
@@ -8,11 +8,16 @@ const mapMovie = movie => ({
 })
 
 module.exports.hello = async (event, context) => {
-  const response = await fetch(`https://yts.am/api/v2/list_movies.json?limit=10&query_term=${event}`)
+  const term = (event && event.query) ? event.query.term : "movie";
+  const response = await fetch(`https://yts.am/api/v2/list_movies.json?limit=10&query_term=${term}`)
     .then(res => res.json())
   return {
     statusCode: 200,
-    body: Object.assign({}, response.data, { movies: response.data.movies.map(mapMovie) }),
-    event: event
+    body: Object.assign({}, response.data, { movies: (response && response.data && response.data.movies) ? response.data.movies.map(mapMovie) : [] }),
+    term: term
   };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
